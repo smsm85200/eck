280fba10f3138477717af9d4bdaf084ead1b14c2
smsm85200 <147330112+smsm85200@users.noreply.github.com>
2023-12-23 02:01:29 +0300

    Add files via upload

---
+++ StringGenBot/start.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+from config import OWNER_ID
+
+
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Client.on_message(filter("start"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""⦿ 𝐓𝐇𝐄 𝐁𝐎𝐓 𝐖𝐀𝐒 𝐂𝐑𝐄𝐀𝐓𝐄𝐃 𝐁𝐘 [ ˛ 𝙃𝙀𝙇𝙇𝘼𝙎 . ](https://t.me/u0uu0)""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="‹ 𝐜𝐥𝐢𝐜𝐤 𝐭𝐨 𝐜𝐫𝐞𝐚𝐭𝐞 𝐚 𝐬𝐞𝐬𝐬𝐢𝐨𝐧 ›", callback_data="generate")
+                ],
+                [
+                    InlineKeyboardButton("‹ ˛ 𝙃𝙀𝙇𝙇𝘼𝙎 . ›", url="https://t.me/u0uu0"),
+                    InlineKeyboardButton("‹ ˛ 𝙃𝙀𝙇𝙇𝘼𝙎 . ›", user_id=OWNER_ID)
+                ]
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
